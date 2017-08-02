10e64f372613e7b61d0c1cab1d02f0323af25264
Koramit Pichanaharee <koramit.p@gmail.com>
2017-08-02 12:27:56 +0700

    add post case closed jobs#7.

--- app/PathoreportLoader.php
+++ app/PathoreportLoader.php
@@ -8,15 +8,18 @@ use \Carbon\Carbon;
 class PathoreportLoader {
     
     protected $snoList;
+    protected $month;
     
-    public function __construct($fileName) {
+    public function __construct($fileName, $month) {
         $this->snoList = loadCSV($fileName);
+        $this->month = $month;
     }
 
     public function loadPathoreports() {
         foreach ($this->snoList as $sno) {
             if ( $sno['HN'] != 0 ) {
-                $case = $this->searchMatchCase($sno);
+                // $case = $this->searchMatchCase($sno);
+                $case = $this->searchMatchCase($sno['HN']);
                 if ( $case != NULL ) {
                     $case->surgical_number = $sno['s_number'];
                     $case->save();
@@ -25,12 +28,21 @@ class PathoreportLoader {
         }
     }
 
-    protected function searchMatchCase(array $sno) {
-        $dateBx = Carbon::createFromFormat('d-m-Y H:i', $sno['date_bx']);
-        $cases = BiopsyCase::where('case_close_status', 1)->where('date_bx', $dateBx->format('Y-m-d'))->get();
+    // protected function searchMatchCase(array $sno) {
+        // $dateBx = Carbon::createFromFormat('d-m-Y H:i', $sno['date_bx']);
+        // $cases = BiopsyCase::where('case_close_status', 1)->where('date_bx', $dateBx->format('Y-m-d'))->get();
+        // if ( $cases != NULL )
+        //     foreach ($cases as $case)
+        //         if ( $case->HN == $sno['HN'] ) return $case;
+
+        // return NULL;
+
+    protected function searchMatchCase($hn) {
+        // $dateBx = Carbon::createFromFormat('d-m-Y H:i', $sno['date_bx']);
+        $cases = BiopsyCase::where('case_close_status', 1)->whereMonth('date_bx', $this->month)->get();
         if ( $cases != NULL )
             foreach ($cases as $case)
-                if ( $case->HN == $sno['HN'] ) return $case;
+                if ( $case->HN == $hn ) return $case;
 
         return NULL;
     }
